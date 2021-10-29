0c098ff5754fc31c02f37cce49e4c7d84c73b6ad
Robn88 <robert.d.nicholas@gmail.com>
2021-10-29 15:10:35 +0000

    Add js to score correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
-/*document.getElementById("start").addEventListener("click", myFunction);
 
-function myFunction() {
-  document.getElementById("start").style.backgroundColor = "red";
-}*/
+
 
 let noESounds = document.getElementsByClassName("noESound");
 let ESounds = document.getElementsByClassName("ESound");
@@ -10,11 +7,36 @@ let ESounds = document.getElementsByClassName("ESound");
 for (let noESound of noESounds) {
   noESound.addEventListener("click", function(){
     this.style.backgroundColor = "red";
+    alert ("Bad luck- this word doesn't contain a long e sound!");
+    incrementWrongAnswer();
+
   })
 }
 
 for (let ESound of ESounds) {
   ESound.addEventListener("click", function(){
     this.style.backgroundColor = "green";
+    alert ("Correct! This word contains a long e sound!");
+    incrementScore();
   })
+}
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+ function incrementScore() {
+
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+
+}
+
+/**
+* Gets the current tally of incorrect answers from the DOM and increments it by 1
+*/
+function incrementWrongAnswer() {
+
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
+  
 }
