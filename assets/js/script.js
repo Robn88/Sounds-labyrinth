a7d7676ef79947b32b9687662f79633b3fca5819
Robn88 <robert.d.nicholas@gmail.com>
2021-11-01 19:36:10 +0000

    Add basic checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 /** Starts the game */
 
+document.getElementById("start").addEventListener("click", function(){
+  document.getElementById("A2").innerHTML = rightAnswers[0];
+  document.getElementById("B1").innerHTML = wrongAnswers[0];
+})
+
+document.getElementById("A2").addEventListener("click", checkAnswer)
+document.getElementById("B1").addEventListener("click", checkAnswer)
+
+
+
 var correctWords = ["even", "Steven", "evil", "meet", "meat", "area", "legal", "be", "achieve", "belief", "sheep", "beat", "free", "clear", "breathe", "leave", "baby", "family", "piece", "any", "beach", "key", "cream", "fear", "recent"];
 var incorrectWords = ["bet", "left", "health", "dead", "breath", "live", "fitness", "bread", "ship", "felt", "England", "July", "fry", "silent", "think", "eleven", "met", "with"];
 
@@ -18,32 +28,24 @@ function getRandom(count, arr){
 }
 
 
-
 let rightAnswers = (getRandom(21,correctWords));
 console.log(rightAnswers)
 console.log(rightAnswers[1]);
 
+let wrongAnswers = (getRandom(15,incorrectWords));
+console.log(wrongAnswers);
+console.log(wrongAnswers[0]);
 
-
-let noESounds = document.getElementsByClassName("noESound");
-let ESounds = document.getElementsByClassName("ESound");
-
-for (let noESound of noESounds) {
-  noESound.addEventListener("click", function(){
-    this.style.backgroundColor = "red";
-    alert ("Bad luck- this word doesn't contain a long e sound!");
-    incrementWrongAnswer();
-
-  })
-}
-
-
-for (let ESound of ESounds) {
-  ESound.addEventListener("click", function(){
+function checkAnswer(){
+  if(element.classList.contains("ESound")) {
     this.style.backgroundColor = "green";
     alert ("Correct! This word contains a long e sound!");
     incrementScore();
-  })
+  } else {
+    this.style.backgroundColor = "red";
+    alert ("Bad luck- this word doesn't contain a long e sound!");
+    incrementWrongAnswer();
+  }
 }
 
 /**
