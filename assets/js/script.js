8a933f080bb14e2e014260ff77d066a815e956a8
Robn88 <robert.d.nicholas@gmail.com>
2021-11-03 14:05:08 +0000

    Add next 5 event listeners and functions to print answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,73 @@
 
 document.getElementById("start").addEventListener("click", function(){
   document.getElementById("a2").innerHTML = rightAnswers[0];
+  document.getElementById("a2").classList.remove("word");
   document.getElementById("b1").innerHTML = wrongAnswers[0];
+  document.getElementById("b1").classList.remove("word");
 })
 
+//add checkAnswer event listeners for cells a2 and b3 and print next two answers
 document.getElementById("a2").addEventListener("click", checkAnswer)
 document.getElementById("a2").addEventListener("click", function(){
   document.getElementById("a3").innerHTML = rightAnswers[1]
+  document.getElementById("a3").classList.remove("word");
   document.getElementById("b2").innerHTML = wrongAnswers[1];
+  document.getElementById("b2").classList.remove("word");
 })
 document.getElementById("b1").addEventListener("click", checkAnswer)
 
+
+//add checkAnswer event listeners for cells a3 and b2 and print next two answers
+document.getElementById("a3").addEventListener("click", checkAnswer)
+document.getElementById("a3").addEventListener("click", function(){
+  document.getElementById("a4").innerHTML = rightAnswers[2]
+  document.getElementById("a4").classList.remove("word");
+  document.getElementById("b3").innerHTML = wrongAnswers[2];
+  document.getElementById("b3").classList.remove("word");
+})
+document.getElementById("b2").addEventListener("click", checkAnswer)
+
+//add checkAnswer event listeners for cells a4 and b3 and print next two answers
+document.getElementById("a4").addEventListener("click", checkAnswer)
+document.getElementById("a4").addEventListener("click", function(){
+  document.getElementById("a5").innerHTML = rightAnswers[3]
+  document.getElementById("a5").classList.remove("word");
+  document.getElementById("b4").innerHTML = wrongAnswers[3];
+  document.getElementById("b4").classList.remove("word");
+})
+document.getElementById("b3").addEventListener("click", checkAnswer)
+
+//add checkAnswer event listeners for cells a5 and b4 and print next two answers
+document.getElementById("a5").addEventListener("click", checkAnswer)
+document.getElementById("a5").addEventListener("click", function(){
+  document.getElementById("a6").innerHTML = wrongAnswers[4]
+  document.getElementById("a6").classList.remove("word");
+  document.getElementById("b5").innerHTML = rightAnswers[4];
+  document.getElementById("b5").classList.remove("word");
+})
+document.getElementById("b4").addEventListener("click", checkAnswer)
+
+//add checkAnswer event listeners for cells a6 and b5 and print next two answers
+document.getElementById("b5").addEventListener("click", checkAnswer)
+document.getElementById("b5").addEventListener("click", function(){
+  document.getElementById("b6").innerHTML = wrongAnswers[5]
+  document.getElementById("b6").classList.remove("word");
+  document.getElementById("c5").innerHTML = rightAnswers[5];
+  document.getElementById("c5").classList.remove("word");
+})
+document.getElementById("a6").addEventListener("click", checkAnswer)
+
+//add checkAnswer event listeners for cells b6 and c5 and print next two answers
+document.getElementById("c5").addEventListener("click", checkAnswer)
+document.getElementById("c5").addEventListener("click", function(){
+  document.getElementById("c6").innerHTML = wrongAnswers[6]
+  document.getElementById("c6").classList.remove("word");
+  document.getElementById("d5").innerHTML = wrongAnswers[7]
+  document.getElementById("d5").classList.remove("word");
+  document.getElementById("c4").innerHTML = rightAnswers[6];
+  document.getElementById("c4").classList.remove("word");
+})
+document.getElementById("b6").addEventListener("click", checkAnswer)
 
 
 var correctWords = ["even", "Steven", "evil", "meet", "meat", "area", "legal", "be", "achieve", "belief", "sheep", "beat", "free", "clear", "breathe", "leave", "baby", "family", "piece", "any", "beach", "key", "cream", "fear", "recent"];
