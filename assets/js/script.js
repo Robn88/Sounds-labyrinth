8f4aa52d26b0276e66cd741efda587f16d04849f
Robn88 <robert.d.nicholas@gmail.com>
2021-11-04 15:52:23 +0000

    Add modal function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -211,7 +211,7 @@ console.log(wrongAnswers[0]);
 function checkAnswer() {
   if (this.classList.contains("ESound")) {
     this.classList.add("greenBackground");
-    alert("Correct! This word contains a long e sound!");
+    openCorrectModal()
     this.classList.add("disable")
     incrementScore();
   } else {
@@ -240,4 +240,28 @@ function incrementWrongAnswer() {
   let oldScore = parseInt(document.getElementById("incorrect").innerText);
   document.getElementById("incorrect").innerText = ++oldScore;
 
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function openCorrectModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
